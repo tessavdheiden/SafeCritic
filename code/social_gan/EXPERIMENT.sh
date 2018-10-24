ddecea6b9e4660d3e4bd37e36fc3d414a0b6b785
tessavdheiden <tessavdheiden@gmail.com>
2018-10-24 10:59:40 +0200

    Add files via upload

--- code/social_gan/EXPERIMENT.sh
+++ code/social_gan/EXPERIMENT.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-python3 scripts/train.py --output_dir 'models/safeGAN_SP_RL' --dataset_name 'bookstore_0' --pooling_dim 2 --pool_static 1 --lamb 1.0 --num_epochs 201 --checkpoint_name 'bookstore_0_12'
+python3 scripts/train.py --output_dir 'models/safeGAN_SP_RL' --dataset_name 'zara_2' --pooling_dim 2 --pool_static 1 --lamb 1.0 --num_epochs 201 --checkpoint_name 'zara_2_12'
+python3 scripts/train.py --output_dir 'models/safeGAN_SP_RL' --dataset_name 'zara_1' --pooling_dim 2 --pool_static 1 --lamb 1.0 --num_epochs 201 --checkpoint_name 'zara_1_12'
+python3 scripts/train.py --output_dir 'models/safeGAN_SP_RL' --dataset_name 'students_3' --pooling_dim 2 --pool_static 1 --lamb 1.0 --num_epochs 201 --checkpoint_name 'students_3_12'
+
 
 
 
