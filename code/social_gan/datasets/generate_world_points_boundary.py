a44676905db11c23c28d23749173fd6af444d315
giuseppeCoccia <giuseppecoccia94@gmail.com>
2018-10-24 15:05:11 +0200

    Update generate_world_points_boundary to let it produce .npy files instead of txt

--- code/social_gan/datasets/generate_world_points_boundary.py
+++ code/social_gan/datasets/generate_world_points_boundary.py
@@ -49,8 +49,6 @@ def generate_boundary_points(data_folder, annotated_image_name, test=False):
         if root != data_folder:
             break;
         for scene_folder in dirs:
-            if scene_folder != "hotel":
-                continue
             annotated_image = load_bin_map(root + scene_folder + annotated_image_name)
             h_matrix = pd.read_csv(root + scene_folder + "/" + scene_folder + "_homography.txt", delim_whitespace=True, header=None).values
 
@@ -71,11 +69,11 @@ def generate_boundary_points(data_folder, annotated_image_name, test=False):
                 ax2.set_xlabel('x-coordinate')
                 plt.show()
 
-            np.savetxt(root + scene_folder + "/world_points_boundary.txt", world_boundary_points, fmt='%.4f')
+            np.save(root + scene_folder + "/world_points_boundary.npy", world_boundary_points)
 
 
 def main():
-    data_folder = '/home/q467565/Desktop/FLORA/code/social_gan/datasets/dataset/ETH/'
+    data_folder = '/home/q467565/Desktop/FLORA/code/social_gan/datasets/safegan_dataset/SDD/'
     annotated_image_name = '/annotated_boundaries.jpg'
     generate_boundary_points(data_folder, annotated_image_name)
     return True
