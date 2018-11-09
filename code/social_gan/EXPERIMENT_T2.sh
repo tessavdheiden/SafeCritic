5c637f50af997f76c22da5a7c911855fa06d3d3f
giuseppeCoccia <giuseppecoccia94@gmail.com>
2018-11-09 15:25:39 +0100

    Correct error in dataset names

--- code/social_gan/EXPERIMENT_T2.sh
+++ code/social_gan/EXPERIMENT_T2.sh
@@ -2,13 +2,13 @@
 
 
 # TABLE 2: safeGAN
-python3 scripts/train.py --output_dir 'models_ucy/safeGAN_DP4' --dataset_name 'sdd' --pooling_dim 4 --pool_static 0 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'local'
-python3 scripts/train.py --output_dir 'models_ucy/safeGAN_SP' --dataset_name 'sdd' --pooling_dim 2 --pool_static 1 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'local'
-python3 scripts/train.py --output_dir 'models_ucy/safeGAN_DP4_SP' --dataset_name 'sdd' --pooling_dim 4 --pool_static 1 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'local'
+python3 scripts/train.py --output_dir 'models_sdd/safeGAN_DP4' --dataset_name 'bookstore_3' --pooling_dim 4 --pool_static 0 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'local'
+python3 scripts/train.py --output_dir 'models_sdd/safeGAN_SP' --dataset_name 'bookstore_3' --pooling_dim 2 --pool_static 1 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'local'
+python3 scripts/train.py --output_dir 'models_sdd/safeGAN_DP4_SP' --dataset_name 'bookstore_3' --pooling_dim 4 --pool_static 1 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'local'
 
 # TABLE 2: socialGAN
-python3 scripts/train.py --output_dir 'models_ucy/socialGAN' --dataset_name 'sdd' --pooling_dim 2 --pool_static 0 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'global'
+python3 scripts/train.py --output_dir 'models_sdd/socialGAN' --dataset_name 'bookstore_3' --pooling_dim 2 --pool_static 0 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'global'
 
 # TABLE 2: socialLSTM
-python3 scripts/train.py --output_dir 'models_ucy/socialLSTM' --dataset_name 'sdd' --pooling_dim 2 --pool_static 0 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'global' --pooling_type 'spool'
+python3 scripts/train.py --output_dir 'models_sdd/socialLSTM' --dataset_name 'bookstore_3' --pooling_dim 2 --pool_static 0 --lamb 0.0 --checkpoint_name 'sdd' --d_type 'global' --pooling_type 'spool'
 
