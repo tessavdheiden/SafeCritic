ecf5e508a92ce84db9b8b742ccc4fad381539a34
giuseppeCoccia <giuseppecoccia94@gmail.com>
2018-11-12 13:35:46 +0100

    Correct errors in remove_edges_boundaries code

--- code/social_gan/datasets/remove_edges_boundaries.py
+++ code/social_gan/datasets/remove_edges_boundaries.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 from datasets.calculate_static_scene_boundaries import get_boundary_points, get_world_from_pixels
 
-dataset_name = "bookstore_0"
-dir_dataset = "safegan_dataset/SDD/" + dataset_name
+
+dir_dataset = "/home/userName/Desktop/FLORA/code/social_gan/datasets/safegan_dataset/SDD/"
 
 
 def remove_borders(boundary_image, border_pixels=10):
@@ -30,24 +30,25 @@ for root, dirs, files in os.walk(dir_dataset):
     if root != dir_dataset:
         break;
     for scene_folder in dirs:
+        print("\n*****scene_folder:", scene_folder)
 
-        boundary_image = plt.imread(dir_dataset + "/" + '{}_annotated_boundaries.jpg'.format(dataset_name))
+        boundary_image = plt.imread(dir_dataset + scene_folder + "/annotated_boundaries.jpg")
         image_without_boundary_points = remove_borders(boundary_image)
         boundary_points = get_boundary_points(image_without_boundary_points)
 
         dset = dir_dataset
-        h = pd.read_csv(dir_dataset + '/{}_homography.txt'.format(dataset_name), delim_whitespace=True, header=None).values
+        h = pd.read_csv(dir_dataset + scene_folder + '/{}_homography.txt'.format(scene_folder), delim_whitespace=True, header=None).values
         world_boundary_points = get_world_from_pixels(boundary_points, h)
         plt.scatter(world_boundary_points[:, 0], world_boundary_points[:, 1], s=1)
 
         # check with coordinates
-        coordinates = np.loadtxt(dir_dataset + "/" + '{}.txt'.format(dataset_name))
+        coordinates = np.loadtxt(dir_dataset + scene_folder + "/{}.txt".format(scene_folder))
         x = coordinates[:, 2]
         y = coordinates[:, 3]
         pts_wrd = np.stack((x, y)).T
         plt.scatter(pts_wrd[:, 0], pts_wrd[:, 1], s=1)
-        plt.show()
-        np.save(dir_dataset + "/" + 'world_points_boundary.npy', world_boundary_points)
+        #plt.show()
+        np.save(dir_dataset + scene_folder + "/world_points_boundary.npy", world_boundary_points)
 
         # plt.show()
         # break
