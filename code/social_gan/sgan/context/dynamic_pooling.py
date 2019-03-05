c344b6da77fe2547b8d3b224593859f501151f37
Tessa van der Heiden <tessavdheiden@gmail.com>
2019-03-05 09:05:26 +0100

    Add files via upload

--- code/social_gan/sgan/context/dynamic_pooling.py
+++ code/social_gan/sgan/context/dynamic_pooling.py
@@ -1,7 +1,12 @@
 import torch
 import torch.nn as nn
+import os
 
 from sgan.mlp import make_mlp
+from sgan.context.physical_attention import Attention_Decoder
+from scripts.visualization import visualize_attention_weights
+from sgan.folder_utils import get_dset_name, get_root_dir, get_test_data_path
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -201,13 +206,149 @@ class SocialPooling(nn.Module):
             offset = self.repeat(offset.view(-1, 1), num_ped).view(-1)
             grid_pos += offset
             grid_pos[within_bound != 0] = 0
-            grid_pos = grid_pos.view(-1, 1).expand_as(curr_hidden_repeat)
+            grid_pos = grid_pos.view(-1, 1).expand_as(curr_hidden_repeat) # grid_pos = [num_ped**2, h_dim]
 
-            curr_pool_h = curr_pool_h.scatter_add(0, grid_pos,
-                                                  curr_hidden_repeat)
+            curr_pool_h = curr_pool_h.scatter_add(0, grid_pos, curr_hidden_repeat) # curr_hidden_repeat = [num_ped**2, h_dim]
             curr_pool_h = curr_pool_h[1:]
             pool_h.append(curr_pool_h.view(num_ped, -1))
 
         pool_h = torch.cat(pool_h, dim=0)
         pool_h = self.mlp_pool(pool_h)
         return pool_h
+
+
+
+class SocialPoolingAttention(nn.Module):
+    """Current state of the art pooling mechanism:
+    http://cvgl.stanford.edu/papers/CVPR16_Social_LSTM.pdf"""
+    def __init__(
+        self, h_dim=64, bottleneck_dim= 1024,activation='relu', batch_norm=True, dropout=0.0,
+        neighborhood_size=2.0, grid_size=8, pool_dim=None
+    ):
+        super(SocialPoolingAttention, self).__init__()
+        self.h_dim = h_dim
+        self.grid_size = grid_size
+        self.neighborhood_size = neighborhood_size
+
+        self.attention_decoder = Attention_Decoder(
+            attention_dim=bottleneck_dim, embed_dim=4, decoder_dim=h_dim, encoder_dim=h_dim)
+
+        mlp_pool_dims = [bottleneck_dim, bottleneck_dim]
+
+        self.mlp_pool = make_mlp(
+            mlp_pool_dims,
+            activation=activation,
+            batch_norm=batch_norm,
+            dropout=dropout
+        )
+
+    def get_bounds(self, ped_pos):
+        top_left_x = ped_pos[:, 0] - self.neighborhood_size / 2
+        top_left_y = ped_pos[:, 1] + self.neighborhood_size / 2
+        bottom_right_x = ped_pos[:, 0] + self.neighborhood_size / 2
+        bottom_right_y = ped_pos[:, 1] - self.neighborhood_size / 2
+        top_left = torch.stack([top_left_x, top_left_y], dim=1)
+        bottom_right = torch.stack([bottom_right_x, bottom_right_y], dim=1)
+        return top_left, bottom_right
+
+    def get_grid_locations(self, top_left, other_pos):
+        cell_x = torch.floor(
+            ((other_pos[:, 0] - top_left[:, 0]) / self.neighborhood_size) *
+            self.grid_size)
+        cell_y = torch.floor(
+            ((top_left[:, 1] - other_pos[:, 1]) / self.neighborhood_size) *
+            self.grid_size)
+        grid_pos = cell_x + cell_y * self.grid_size
+        return grid_pos
+
+    def repeat(self, tensor, num_reps):
+        """
+        Inputs:
+        -tensor: 2D tensor of any shape
+        -num_reps: Number of times to repeat each row
+        Outpus:
+        -repeat_tensor: Repeat each row such that: R1, R1, R2, R2
+        """
+        col_len = tensor.size(1)
+        tensor = tensor.unsqueeze(dim=1).repeat(1, num_reps, 1)
+        tensor = tensor.view(-1, col_len)
+        return tensor
+
+
+    def forward(self, h_states, seq_start_end, end_pos, rel_pos, seq_scene_ids=None):
+        """
+        Inputs:
+        - h_states: Tesnsor of shape (num_layers, batch, h_dim)
+        - seq_start_end: A list of tuples which delimit sequences within batch.
+        - end_pos: Absolute end position of obs_traj (batch, 2)
+        Output:
+        - pool_h: Tensor of shape (batch, h_dim)
+        """
+        pool_h = []
+        for i, (start, end) in enumerate(seq_start_end):
+            start = start.item()
+            end = end.item()
+            num_ped = end - start
+            grid_size = self.grid_size * self.grid_size
+            curr_hidden = h_states.view(-1, self.h_dim)[start:end]
+            curr_hidden_repeat = curr_hidden.repeat(num_ped, 1)
+            curr_end_pos = end_pos[start:end]
+            curr_pool_h_size = (num_ped * grid_size) + 1
+            curr_pool_h = curr_hidden.new_zeros((curr_pool_h_size, self.h_dim))
+            # curr_end_pos = curr_end_pos.data
+            top_left, bottom_right = self.get_bounds(curr_end_pos)
+
+            # Used in attention
+            embed_info = torch.cat([curr_end_pos, rel_pos[start:end]], dim=1)
+
+            # Repeat position -> P1, P2, P1, P2
+            curr_end_pos = curr_end_pos.repeat(num_ped, 1)
+            # Repeat bounds -> B1, B1, B2, B2
+            top_left = self.repeat(top_left, num_ped)
+            bottom_right = self.repeat(bottom_right, num_ped)
+
+            grid_pos = self.get_grid_locations(
+                    top_left, curr_end_pos).type_as(seq_start_end)
+            # Make all positions to exclude as non-zero
+            # Find which peds to exclude
+            x_bound = ((curr_end_pos[:, 0] >= bottom_right[:, 0]) +
+                       (curr_end_pos[:, 0] <= top_left[:, 0]))
+            y_bound = ((curr_end_pos[:, 1] >= top_left[:, 1]) +
+                       (curr_end_pos[:, 1] <= bottom_right[:, 1]))
+
+            within_bound = x_bound + y_bound
+            within_bound[0::num_ped + 1] = 1  # Don't include the ped itself
+            within_bound = within_bound.view(-1)
+
+            # This is a tricky way to get scatter add to work. Helps me avoid a
+            # for loop. Offset everything by 1. Use the initial 0 position to
+            # dump all uncessary adds.
+            grid_pos += 1
+            total_grid_size = self.grid_size * self.grid_size
+            offset = torch.arange( 0, total_grid_size * num_ped, total_grid_size ).type_as(seq_start_end)
+
+            offset = self.repeat(offset.view(-1, 1), num_ped).view(-1)
+            grid_pos += offset
+            grid_pos[within_bound != 0] = 0
+            grid_pos = grid_pos.view(-1, 1).expand_as(curr_hidden_repeat) # grid_pos = [num_ped**2, h_dim]
+
+            curr_pool_h = curr_pool_h.scatter_add(0, grid_pos, curr_hidden_repeat) # curr_hidden_repeat = [num_ped**2, h_dim]
+            curr_pool_h = curr_pool_h[1:]
+
+            encoder_out = curr_pool_h.view(num_ped, total_grid_size, self.h_dim)
+            curr_pool_h, attention_weights = self.attention_decoder(encoder_out=encoder_out, curr_hidden=curr_hidden, embed_info=embed_info)
+
+            '''
+            data_dir = get_test_data_path('sdd')
+            list_data_files = sorted([get_dset_name(os.path.join(data_dir, _path).split("/")[-1]) for _path in os.listdir(data_dir)])
+            seq_scenes = [list_data_files[num] for num in seq_scene_ids]
+            visualize_attention_weights(seq_scenes[i], self.grid_size, attention_weights, end_pos[start:end])
+            '''
+
+            pool_h.append(curr_pool_h.view(num_ped, -1)) # grid_size * grid_size * h_dim
+
+        pool_h = torch.cat(pool_h, dim=0)
+        pool_h = self.mlp_pool(pool_h)
+        return pool_h
+
+
