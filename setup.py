da90c1ddb26991261c278f3944a31829aaf09650
Tessa Heiden <tessa.heiden@bmw.de>
2019-06-06 13:08:35 +0200

    added last modifications

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ setup(
     version='0.0.1',
     packages=[
 	'sgan',
+	'sgan.model',
+	'sgan.evaluation',
+	'sgan.context',
+	'sgan.model',
 	'scripts',
+	'scripts.helpers',
+	'scripts.evaluation'
     ],
     install_requires=[
         'matplotlib',
-        'numpy',
         'torch',
 	'torchvision',
+	'numpy==1.15.0'
     ],
 )
