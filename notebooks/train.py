1d80f73150f3ed835a56bae87288e824c05e094e
Student <christian.wc.weiss@gmail.com>
2019-07-10 15:41:00 +0200

    Adjustments to training routine

--- notebooks/train.py
+++ notebooks/train.py
@@ -1,3 +1,6 @@
+import io
+from statistics import mean
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,63 +8,78 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 import data.Loader as ld
 import model.DeconvNet as md
 import support.colors as colors
 
-batch_size = 4
+def weights_init(m):
+    if isinstance(m, nn.Conv2d):
+        nn.init.xavier_uniform_(m.weight.data)
+    elif isinstance(m, nn.ConvTranspose2d):
+        nn.init.xavier_uniform_(m.weight.data)
+
+batch_size = 8
 workers = 1
 
 train_data = ld.get_data()
 train_data_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=workers)
 net = md.DeconvNet()
+net.apply(weights_init)
 
-#Parameters for Training
-n_epochs = 1
-learning_rate = 0.001
-weight_decay = 0.0005
+# Parameters for Training
+n_epochs = 2
+learning_rate = 0.01
+weight_decay = 0.0000
+momentum = 0.9
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+crit = nn.NLLLoss2d()
+optimizer_adam = optim.Adam(net.parameters(), lr=learning_rate)
+optimizer_sgd = optim.SGD(net.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
 
-def trainNet(net, train_data_loader, criterion, optimizer, n_epochs, learning_rate):
-    
-    #Print all of the hyperparameters of the training iteration:
-    print("===== HYPERPARAMETERS =====")
-    print("batch_size=", batch_size)
-    print("epochs=", n_epochs)
-    print("learning_rate=", learning_rate)
-    print("weight_decay=", weight_decay)
-    print("=" * 30)
-    
-    loss_list = []
-    acc_list = []
-    
-    for i, (input_img, output_img) in enumerate(train_data_loader):
-        # Run the forward pass
-        outputs = net(input_img)
-        loss = criterion(outputs, output_img)
-        print(loss)
-        # Backprop and perform Adam optimisation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+# delete after we found bug
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
-        # Track the accuracy
-        total = output_img.size(0)
-        _, predicted = torch.max(outputs.data, 1)
-        correct = (predicted == output_img).sum().item()
+def trainNet(net, train_data_loader, criterion, optimizer, n_epochs):
+    for epoch in range(n_epochs):
+        losses = []
+        for (input_img, target_img) in tqdm(train_data_loader):
+            optimizer.zero_grad()
+            outputs = net(input_img)
+            loss = criterion(input=outputs, target=target_img)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+            print(loss)
+        epoch_loss = mean(losses)
+        print(f"Epoch {epoch+1}/{n_epochs}. Loss: {epoch_loss}.", flush=True)
 
-        print(loss)
-    print("Finished Training")
+if __name__ == "__main__":
+    PATH = "/home/student/Documents/FLORA/notebooks/state_dict.pth"
+    net.load_state_dict(torch.load(PATH))
+    print("Training started...", flush=True)
+    trainNet(net=net,
+             train_data_loader=train_data_loader,
+             criterion=criterion,
+             optimizer=optimizer_adam,
+             n_epochs=n_epochs)
+    print("Training finished.", flush=True)
+    torch.save(net.state_dict(), PATH)
+    print("Model saved..")
+    img_no = 2
+    inp = train_data_loader.dataset.__getitem__(img_no)[0].unsqueeze(0)
+    plt.imshow(train_data_loader.dataset.__getitem__(img_no)[0].permute(1, 2, 0))
+    out = net(inp)
+    print("Output....")
+    print(out)
+    om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
+    print("After Argmax....")
+    print(om)
+    rgb = colors.decode_segmap(om)
+    plt.imshow(rgb)
+    plt.axis('off')
+    plt.show()
 
-trainNet(net=net, train_data_loader=train_data_loader, criterion=criterion, optimizer=optimizer, n_epochs=n_epochs, learning_rate=learning_rate)
 
-inp = train_data_loader.dataset.__getitem__(2)[0].unsqueeze(0)
-out = net(inp)
-print(out)
-om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
-rgb = colors.decode_segmap(om)
-plt.imshow(rgb)
-plt.axis('off')
-plt.show()
+
