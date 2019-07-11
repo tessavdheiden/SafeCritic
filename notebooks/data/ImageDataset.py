10074c74ed0d6842f23e729db10b8315c0993168
Student <christian.wc.weiss@gmail.com>
2019-07-11 16:17:30 +0200

    Load image as greyscale

--- notebooks/data/ImageDataset.py
+++ notebooks/data/ImageDataset.py
@@ -28,27 +28,15 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
         
         input_img = plt.imread(self.X[idx])
-        target_img = plt.imread(self.y[idx])
+        target_img = cv2.imread(self.y[idx], cv2.IMREAD_GRAYSCALE)
 
-        #target_img_instance_id_name = os.path.join(self.root_dir, self.cityscape_frames_Y.iloc[idx, 1])
-        #target_img_label_id_name = os.path.join(self.root_dir, self.cityscape_frames_Y.iloc[idx, 2])
-        #output_json_name = os.path.join(self.root_dir, self.cityscape_frames_Y.iloc[idx, 3])
-            
         input_img = cv2.resize(input_img, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
         target_img = cv2.resize(target_img, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
-        
-        input_img = torch.from_numpy(input_img)
-        target_img = torch.from_numpy((target_img*30).round()).to(torch.long)
+
+        input_img = torch.from_numpy(input_img).to(torch.float32)
+        target_img[target_img >= 30] = 0
+        target_img = torch.from_numpy((target_img)).to(torch.long)
         input_img = input_img.permute(2,0,1)
-        #target_img = target_img.permute(2,0,1)
-
-        #if target_img.shape[0] == 4:
-            #convert the image from RGBA2RGB
-        #    target_img = cv2.cvtColor(target_img, cv2.COLOR_RGBA2RGB)
-
-        #if self.transform:
-         #   input_img = self.transform(input_img)
-        #    target_img = self.transform(target_img)
 
         data = input_img, target_img
         return data
