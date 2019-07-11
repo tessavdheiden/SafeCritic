22795474de373bd11bc303d72177108642ae0fbe
Student <christian.wc.weiss@gmail.com>
2019-07-11 16:19:02 +0200

    Adjusted hyperparameters for training

--- notebooks/train.py
+++ notebooks/train.py
@@ -1,24 +1,17 @@
-import io
 from statistics import mean
+import torch.cuda as cuda
 
-import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import data.Loader as ld
 import model.DeconvNet as md
 import support.colors as colors
-
-def weights_init(m):
-    if isinstance(m, nn.Conv2d):
-        nn.init.xavier_uniform_(m.weight.data)
-    elif isinstance(m, nn.ConvTranspose2d):
-        nn.init.xavier_uniform_(m.weight.data)
 
 batch_size = 8
 workers = 1
@@ -26,20 +19,14 @@ workers = 1
 train_data = ld.get_data()
 train_data_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=workers)
 net = md.DeconvNet()
-net.apply(weights_init)
-
 # Parameters for Training
-n_epochs = 2
+n_epochs = 12
 learning_rate = 0.01
-weight_decay = 0.0000
+weight_decay = 0.0005
 momentum = 0.9
 criterion = nn.CrossEntropyLoss()
-crit = nn.NLLLoss2d()
-optimizer_adam = optim.Adam(net.parameters(), lr=learning_rate)
-optimizer_sgd = optim.SGD(net.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
-
-# delete after we found bug
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+# optimizer = optim.SGD(net.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
+optimizer = optim.Adam(net.parameters(), lr=learning_rate)
 
 def trainNet(net, train_data_loader, criterion, optimizer, n_epochs):
     for epoch in range(n_epochs):
@@ -51,28 +38,25 @@ def trainNet(net, train_data_loader, criterion, optimizer, n_epochs):
             loss.backward()
             optimizer.step()
             losses.append(loss.item())
-            print(loss)
         epoch_loss = mean(losses)
         print(f"Epoch {epoch+1}/{n_epochs}. Loss: {epoch_loss}.", flush=True)
 
 if __name__ == "__main__":
-    PATH = "/home/student/Documents/FLORA/notebooks/state_dict.pth"
+    PATH = "/home/student/Documents/FLORA/notebooks/state_dict_test.pth"
     net.load_state_dict(torch.load(PATH))
     print("Training started...", flush=True)
     trainNet(net=net,
-             train_data_loader=train_data_loader,
-             criterion=criterion,
-             optimizer=optimizer_adam,
-             n_epochs=n_epochs)
+            train_data_loader=train_data_loader,
+            criterion=criterion,
+            optimizer=optimizer,
+            n_epochs=n_epochs)
     print("Training finished.", flush=True)
     torch.save(net.state_dict(), PATH)
     print("Model saved..")
-    img_no = 2
+    img_no = 1
     inp = train_data_loader.dataset.__getitem__(img_no)[0].unsqueeze(0)
     plt.imshow(train_data_loader.dataset.__getitem__(img_no)[0].permute(1, 2, 0))
     out = net(inp)
-    print("Output....")
-    print(out)
     om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
     print("After Argmax....")
     print(om)
